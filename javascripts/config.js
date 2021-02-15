10e8a97202f2410f8e0d10949e2ac04a88ce80dc
Guilherme Marmerola <guilherme.marmerola@loft.com.br>
2021-02-15 18:43:54 -0300

    Deployed f9c7ee4 with MkDocs version: 1.1.2

---
+++ javascripts/config.js
@@ -0,0 +1,14 @@
+hljs.initHighlighting()
+
+window.MathJax = {
+    tex: {
+      inlineMath: [["\\(", "\\)"]],
+      displayMath: [["\\[", "\\]"]],
+      processEscapes: true,
+      processEnvironments: true
+    },
+    options: {
+      ignoreHtmlClass: ".*|",
+      processHtmlClass: "arithmatex"
+    }
+  };
