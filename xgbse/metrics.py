Fix computation maximum deviation D calibration

xgbse/metrics.py
@@ -290,7 +290,7 @@ def dist_calibration_score(y_true, survival, n_bins=10, returns="pval"):
 
     elif returns == "max_deviation":
         proportions = final_bin_counts / final_bin_counts.sum()
-        return np.abs(proportions - 0.1).max()
+        return np.abs(proportions - 1/n_bins).max()
 
     elif returns == "histogram":
         return final_bin_counts
@@ -298,7 +298,7 @@ def dist_calibration_score(y_true, survival, n_bins=10, returns="pval"):
     elif returns == "all":
         result = chisquare(final_bin_counts)
         proportions = final_bin_counts / final_bin_counts.mean()
-        max_deviation = np.abs(proportions - 1).max()
+        max_deviation = np.abs(proportions - 1/n_bins).max()
         return {
             "statistic": result.statistic,
             "pval": result.pvalue,