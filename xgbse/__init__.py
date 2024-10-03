1887906ae77e7a62098454b1779e6fba2a0c9008
Gabriel Gimenez <gabriel.gimenez@loft.com.br>
2024-10-03 14:01:11 -0300

    fix convert signature (#80)
    * fix convert signature
    
    * bump version

--- xgbse/__init__.py
+++ xgbse/__init__.py
@@ -5,7 +5,7 @@ from ._kaplan_neighbors import XGBSEKaplanNeighbors, XGBSEKaplanTree
 from ._meta import XGBSEBootstrapEstimator
 from ._stacked_weibull import XGBSEStackedWeibull
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 __all__ = [
     "XGBSEDebiasedBCE",
